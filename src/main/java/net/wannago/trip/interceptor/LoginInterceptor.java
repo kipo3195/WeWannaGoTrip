28d4698a439603e2525ece0e35662d7b081e96d7
home <home@DESKTOP-H89UMDP>
2021-02-11 23:16:20 +0900

    20210211 interceptor-context 수정! security 다 삭제 차정원....

--- src/main/java/net/wannago/trip/interceptor/LoginInterceptor.java
+++ src/main/java/net/wannago/trip/interceptor/LoginInterceptor.java
@@ -1,19 +1,26 @@
 package net.wannago.trip.interceptor;
 
 
+import javax.inject.Inject;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import net.wannago.member.service.MemberService;
 import net.wannago.member.vo.LoginDTO;
 import net.wannago.member.vo.MemberVO;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter{
 
+	@Inject
+	MemberService ms;
+	
+	
 	// 처음 로그인 : 리턴한 모델의 값을 가지고 확인 해줘야하니, postHandle
 	@Override
 	public void postHandle(HttpServletRequest request, 
@@ -22,19 +29,20 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 						   ModelAndView mav) throws Exception {
 		
 		LoginDTO dto = (LoginDTO)mav.getModel().get("loginDTO");
-		MemberVO vo = (MemberVO)mav.getModel().get("memberInfo");
-		
+		MemberVO vo = (MemberVO)mav.getModel().get("memberInfo"); 
+		 
 		System.out.println("LodingIntercepttor : "+dto);
 		System.out.println("LodingIntercepttor : "+vo);
 		// 값이 들어가있으면 아이디와 비밀번호 일치한걸 넘겨주겠지
 		
-		
-		if(vo != null) { // 로그인 성공
+	
+		if(vo != null  ) { // 로그인 성공
+			
 			HttpSession session = request.getSession();
 			session.setAttribute("memberInfo", vo);
 			 // 로그인정보를 session에 넣고. MemberController에 있는 로그인 mvc를 가져옴.
 			
-				if(dto.isMemberCookie()) {
+				if(dto.isMemberCookie() ) {
 					Cookie cookie = new Cookie("signInCookie",vo.getMid());
 					cookie.setPath("/");
 					cookie.setMaxAge(60 * 60 * 24 * 15);
